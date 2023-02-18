5206f1a84721b02a3f9b3c2ff1c75d709c187626
sotakoyama <koyama.ryomo@gmail.com>
2023-02-18 16:42:01 +0900

    グラフとカレンダーの日本語対応をした。

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,8 +1,8 @@
 <template>
-  
-    <Line :data="chartdata" :options="options" :width="1400" :height="500"/>
-    <button @click="postStockInfoDummy">モックAPI</button>
-
+    <div id="graph-area" class="border rounded-md p-10 mb-10">
+      <Line :data="chartdata" :options="options" :width="1800" :height="500"/>
+    </div>
+    
 
     <div id="form" class="border rounded-md p-4 w-6/12 mx-auto">
       <div class="mb-4">
@@ -17,7 +17,9 @@
           取得開始日時
         </label>
         <Datepicker class="appearance-none w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        v-model="pickedFrom" id="pickedFrom" 
+        v-model="pickedFrom"
+        id="pickedFrom" 
+        :locale="locale"
         :upperLimit="pickedTo"/>
       </div>
       <div class="mb-4">
@@ -25,18 +27,22 @@
           取得期限日時
         </label>
           <Datepicker class="appearance-none w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-            v-model="pickedTo" id="pickedTo" 
+            v-model="pickedTo" 
+            id="pickedTo" 
+            :locale="locale"
             :lowerLimit="pickedFrom"/>
       </div>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+      type="submit"
+      @click="postStockInfoDummy">
         株価の取得
       </button>
     </div>
-    
-
+  
 </template>
 
 <script setup>
+import { ja } from 'date-fns/locale'
 import axios from "axios";
 import Datepicker from "vue3-datepicker"
 
@@ -92,6 +98,9 @@ const options = ref({
     }
   })
 
+const locale= ref(ja)
+
+
 async function postStockInfoDummy() {
   try {
     var jsonString = '{"1641340800000":{"Open":179.6100006104,"High":180.1699981689,"Low":174.6399993896,"Close":174.9199981689,"Adj Close":173.6455383301,"Volume":94537600},"1641427200000":{"Open":172.6999969482,"High":175.3000030518,"Low":171.6399993896,"Close":172.0,"Adj Close":170.7468109131,"Volume":96904000},"1641513600000":{"Open":172.8899993896,"High":174.1399993896,"Low":171.0299987793,"Close":172.1699981689,"Adj Close":170.9155883789,"Volume":86709100}}';
@@ -110,7 +119,14 @@ async function postStockInfoDummy() {
     for (var date in jsonData) {
       if (jsonData.hasOwnProperty(date)) {
         var data = jsonData[date];
-        labels.push(new Date(parseInt(date)).toDateString())
+        labels.push(new Date(parseInt(date)).toLocaleString('ja-JP', {
+          timeZone: 'Asia/Tokyo',
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour12: false,
+        }))
+        
         dataOpne.push(data.Open);
         dataHigh.push(data.High);
         dataLow.push(data.Low);
@@ -170,13 +186,10 @@ async function postStockInfoDummy() {
         },
         title: {
           display: true,
-          text: 'Apple' + '株価チャート'
+          text: symbol.value + '　株価チャート'
         }
       }
-    }
-
-    
-    console.log("complete")
+    }   
   } catch (e) {
     console(e)
   }finally{
